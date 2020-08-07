38bbad0cf8ebe40602abc8ece150ebed4f2077c3
Tatjana Devrnja <devrnjat94@gmail.com>
2020-08-07 18:06:47 +0200

    made loading indicator and validation errors

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-import User from './User.js'
+
 import AuthValidator from './AuthValidator.js'
+
 
 let validator = new AuthValidator();
 document.getElementById('btnClick').onclick = onSubmit;
 
+let email_error = document.getElementById('email_error');
+let pass_error = document.getElementById('pass_error');
+
 function onSubmit(){
+
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
+    let spinner = document.getElementById('spinner');
 
-    if(email == validator.validateEmail(email)){
-        
-        if(password == validator.validatePassword(password)){
-            alert("You are logged in!");
+    spinner.classList.add('spinner');
+
+    setTimeout(() => {
+        try {
+            spinner.classList.remove('spinner');
+            validator.validateEmail(email);
+            
+        } catch (error) {
+            
+            email_error.classList.remove("hidden");
+            document.getElementById('email_error').innerHTML = error.message;
+            return false;
         }
-    }
+        try {
+            validator.validatePassword(password);
+            alert ("login");
+
+        } catch (error) {
+            pass_error.classList.remove("hidden");
+            document.getElementById('pass_error').innerHTML = error.message;
+            return false;
+        }       
+    }, 2000);   
 }
 
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden"; 
-});
+function removeEmailError(){
+    email_error.classList.add("hidden");
+}
+
+document.getElementById('email').addEventListener('input', removeEmailError);
+
+function removePasswordError(){
+    pass_error.classList.add("hidden");
+}
+document.getElementById('password').addEventListener('input', removePasswordError);
