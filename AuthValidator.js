54cbe0a4dba35eec19f923a18d0e0f71222a25a1
Tatjana Devrnja <devrnjat94@gmail.com>
2020-08-18 19:34:21 +0200

    validateConfirmPassword

--- AuthValidator.js
+++ AuthValidator.js
@@ -1,39 +1,56 @@
-import ValidationError from './ValidationError.js'
+import ValidationError from "./ValidationError.js";
 
 export default class AuthValidator {
+  constructor() {}
 
-    constructor() {}
-
-    validateEmail(email) {
-        if (!email.includes("@") || email.length < 10) {
-            throw new ValidationError(`Email should contain "@" and should be at least 10 characters long.`);
-        } else {
-            return email;
-        }
+  validateEmail(email) {
+    if (!email.includes("@") || email.length < 10) {
+      throw new ValidationError(
+        `Email should contain "@" and should be at least 10 characters long.`
+      );
+    } else {
+      return email;
     }
+  }
 
-    validatePassword(password) {
-        let minPasswordLength = 3;
-        let alphanumericChars = /[a-z0-9]+/i
-        let specialChars = /[!@#$%^&*(),.?":{}|<>]/;
+  validatePassword(password) {
+    let minPasswordLength = 3;
+    let alphanumericChars = /[a-z0-9]+/i;
+    let specialChars = /[!@#$%^&*(),.?":{}|<>]/;
 
-        if (password.length < minPasswordLength) {
-            throw new ValidationError(`Password is too short. It should be at least ${minPasswordLength} characters long`);
-        } else if (!password.match(alphanumericChars) || !password.match(specialChars)) {
-            throw new ValidationError("Password should include at least 1 alphanumerical and 1 special character");
-        } else {
-            return password;
-        }
+    if (password.length < minPasswordLength) {
+      throw new ValidationError(
+        `Password is too short. It should be at least ${minPasswordLength} characters long`
+      );
+    } else if (
+      !password.match(alphanumericChars) ||
+      !password.match(specialChars)
+    ) {
+      throw new ValidationError(
+        "Password should include at least 1 alphanumerical and 1 special character"
+      );
+    } else {
+      return password;
     }
+  }
 
-    validateFirstName(firstName) {
-        let minNameLength = 3;
+  validateFirstName(firstName) {
+    let minNameLength = 3;
 
-        if (firstName.length < minNameLength) {
-            throw new ValidationError(`Name is too short. It should be at least ${minNameLength} characters long.`);
-        } else {
-            return firstName;
-        }
+    if (firstName.length < minNameLength) {
+      throw new ValidationError(
+        `Name is too short. It should be at least ${minNameLength} characters long.`
+      );
+    } else {
+      return firstName;
     }
+  }
+
+  validateConfirmPassword(password, confirmPassword) {
+    if (password !== confirmPassword) {
+      throw new ValidationError("Passwords do not match!");
+    } else {
+      return confirmPassword;
+    }
+  }
 }
-
